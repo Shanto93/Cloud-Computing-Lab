ce2557f4dab920a4cb4fa5aa6867df8e8149b637
Shanto Islam <shantoislam7363@gmail.com>
2023-06-12 23:56:52 +0600

    Code Uploaded

--- matrixMultiplication.py
+++ matrixMultiplication.py
@@ -6,37 +6,27 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    # Program to multiply two matrices using nested loops
+@app.route('/matrix')
+def matrixMultiplication():
+    a = [[4, 23, 7],
+         [4, 9, 6],
+         [2, 5, 5]]
+    b = [[4, 9, 1, 2],
+         [6, 7, 3, 0],
+         [4, 5, 23, 1]]
+    c = [[0, 0, 0, 0],
+              [0, 0, 0, 0],
+              [0, 0, 0, 0]]
+    la = len(a)
+    lb = len(b)
+    lb0 = len(b[0])
+    for i in range(la):
+        for j in range(lb0):
+            for k in range(lb):
+                c[i][j] += a[i][k] * b[k][j]
 
-    # 3x3 matrix
-    X = [[12,7,3],
-        [4 ,5,6],
-        [7 ,8,9]]
-    # 3x4 matrix
-    Y = [[5,8,1,2],
-        [6,7,3,0],
-        [4,5,9,1]]
-    # result is 3x4
-    result = [[0,0,0,0],
-              [0,0,0,0],
-              [0,0,0,0]]
-
-    # iterate through rows of X
-    for i in range(len(X)):
-     # iterate through columns of Y
-     for j in range(len(Y[0])):
-       # iterate through rows of Y
-       for k in range(len(Y)):
-           result[i][j] += X[i][k] * Y[k][j]
-
-    return result
+    return c
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8085, debug=True)
