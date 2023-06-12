add8e162872ca8fe414c2fc3988026933d2811eb
Shanto Islam <shantoislam7363@gmail.com>
2023-06-13 00:14:40 +0600

    Code Uploaded

--- evenNumber.py
+++ evenNumber.py
@@ -1,19 +1,17 @@
 from flask import  Flask, request, jsonify
 
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 
-@app.route("/<int:n>")
-def hello(n):
+@app.route("/")
+def hello():
+    n = int(input("Input number n: "))
     a = ""
     for num in range(2, 2*n+1, 2):
         if (num%2 == 0) :
             a = a + " "+ str(num)
     return a
 
-
-
-
-if __name__ == "__main__":
+if _name_ == "_main_":
     app.run(host="127.0.0.1", port=8082, debug=True)
